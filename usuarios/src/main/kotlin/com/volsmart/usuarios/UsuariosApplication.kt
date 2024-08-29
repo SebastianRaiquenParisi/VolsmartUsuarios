ffd34fdb05818286ea5b78931c4bf8ce415106e6
SebastianRaiquenParisi <sebastianrparisi@gmail.com>
2024-08-29 14:57:07 -0300

    Proyecto inicializado con spring.io, lenguaje Kotlin, con dependencias de kafka y mongodb

---
+++ usuarios/src/main/kotlin/com/volsmart/usuarios/UsuariosApplication.kt
@@ -0,0 +1,11 @@
+package com.volsmart.usuarios
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsuariosApplication
+
+fun main(args: Array<String>) {
+	runApplication<UsuariosApplication>(*args)
+}
